f607a8f0125df9d2b65bd8a721c6c800956fe3a9
Jebb Domingo <jebb@timble.net>
2017-10-27 08:34:31 +0800

    Fix absolute path

--- code/administrator/components/com_qbsync/database/behavior/syncable.php
+++ code/administrator/components/com_qbsync/database/behavior/syncable.php
@@ -152,7 +152,7 @@ class ComQbsyncDatabaseBehaviorSyncable extends KDatabaseBehaviorAbstract
 
         if ($context->affected !== false && $data->isQboConnected())
         {
-            $config      = $this->getObject('com:rewardlabs.accounting.data');
+            $config      = $this->getObject('com://site/rewardlabs.accounting.data');
             $ItemService = new QuickBooks_IPP_Service_Item();
 
             $Item = new QuickBooks_IPP_Object_Item();
